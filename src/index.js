b23e597cffbc5494b85e6831420650b0ae25febf
Lathashree01 <lathashreeharish01@gmail.com>
2020-10-31 20:33:52 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
